13238ad3f39cb90d3af4a84be5c796926b68c463
hisahisa <jxrku23@hotmail.com>
2023-11-15 17:11:53 +0900

    feat: reduce memory function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // Rust code (src/lib.rs)
 mod structual;
 
+use std::io::Read;
 use std::thread;
 use std::sync::mpsc;
 use std::sync::mpsc::{Sender, Receiver};
@@ -10,6 +11,8 @@ use inflate::inflate_bytes_zlib;
 use quick_xml::Reader;
 use quick_xml::events::{BytesText, Event};
 use chrono::NaiveDateTime;
+use flate2::read::ZlibDecoder;
+
 use crate::structual::StructCsv;
 
 
@@ -34,11 +37,9 @@ impl DataGenerator  {
         let content = content_.to_vec();
         let str_content = str_content_.to_vec();
         let e_msg = "failed to decompress content";
-        let decompress = inflate_bytes_zlib(&content).map_err(
-            |e| PyValueError::new_err(format!("{}: {}", &e_msg, e)))?;
         let decompress_str = inflate_bytes_zlib(&str_content).map_err(
             |e| PyValueError::new_err(format!("{}: {}", &e_msg, e)))?;
-        self._process_bytes(chunk, decompress, decompress_str)
+        self._process_bytes(chunk, content, decompress_str);
     }
 
     fn process_bytes(&self, chunk: u32, content_: &[u8], str_content_: &[u8]) -> PyResult<()> {
@@ -58,56 +59,158 @@ impl DataGenerator  {
 
         // tag "dimension ref=\"xx:yy\"/" の属性取得
         let dimension_tag = vec![100, 105, 109, 101, 110, 115, 105, 111, 110];
+        let target_terminal_vec: Vec<u8> = vec![60, 47, 114, 111, 119, 62]; // </row> tag
 
         let navi = create_navi();
         let tx1 = mpsc::Sender::clone(&self.pro);
         let mut count = 0;
         let closure = move || -> Result<(), PyErr>{
+            let mut first_out = Vec::<u8>::new();
+            let mut second_out = Vec::<u8>::new();
+            let mut decoder = ZlibDecoder::new(content.as_slice());
+            let target_len = 100;
+            let mut is_upper = true;
+            let mut is_last = false;
             let mut width_len: Option<usize> = None;
-            let mut xml_reader = Reader::from_reader(content.as_ref());
+            let mut buffer_inner = Vec::new();
+            //let mut xml_reader: Option<Reader<_>> = None;
+            //let mut x: Option<Vec<u8>> = None;
             loop {
-                match xml_reader.read_event_into(&mut buffer) {
-                    Ok(Event::Start(e)) => {
-                        match e.name().as_ref() {
-                            b"c" => {
-                                let mut struct_csv = StructCsv::new();
-                                for i in e.attributes() {
-                                    match i {
-                                        Ok(x) => {
-                                            match &x.key.into_inner() {
-                                                [115u8] | [116u8] => { // b"s", b"t"
-                                                    struct_csv.set_attr(
-                                                        x.key.into_inner()[0].clone());
-                                                }
-                                                [114u8] => { // b"r"
-                                                    let a = String::from_utf8_lossy(
-                                                        &*x.clone().value.into_owned()).
-                                                        into_owned();
-                                                    let b = column_to_number(a)?;
-                                                    struct_csv.set_r_attr_v(b - 1);
-                                                }
-                                                _ => {}
-                                            }
-                                            s = Some(struct_csv.clone());
-                                        }
-                                        Err(_) => {}
-                                    }
-                                }
-                            },
-                            b"v" => is_v = true,
-                            _ => {},
-                        }
+
+
+                if is_last{
+                    tx1.send(String::from("finish")).unwrap();
+                    break;
+                }
+
+                let mut buffer_outer = vec![0; target_len];
+                let bytes_read = match decoder.read(&mut buffer_outer) {
+                    Ok(x) => {
+                        println!("buffer_outer = {:?}", String::from_utf8_lossy(&buffer_outer));
+                        x
                     }
-                    Ok(Event::End(e)) => {
-                        match e.name().as_ref() {
-                            b"row" => {
-                                let i = row_a.into_iter().map(|a| {
-                                    match a {
-                                        Some(s) => s.clone().
-                                            get_value(&navi, &name_resolve),
-                                        None => "".to_string()
+                    Err(e) => {
+                        println!("e = {:?}", e);
+                        0
+                    }
+                };
+                println!("bytes_read = {:?}", &bytes_read);
+                // let bytes_read = decoder.read(&mut buffer_outer).unwrap();
+
+                let x: Option<Vec<u8>> = if bytes_read == 0 {
+                    let a = if is_upper {
+                        first_out.clone()
+                    }else {
+                        second_out.clone()
+                    };
+                    is_last = true;
+                    let op_index = find_vec_index_rev(&a, &target_terminal_vec);
+                    match op_index {
+                        Some(i) => {
+                            let b = &a[0..i+6];
+                            Some(b.to_vec())
+                        },
+                        None => None
+                    }
+                }
+                else if let Some(index) = find_vec_index_rev(&buffer_outer, &target_terminal_vec) {
+                    if is_upper {
+                        first_out.extend(&buffer_outer[0..&index+6]);
+                        second_out = Vec::<u8>::new();
+                        second_out.extend(&buffer_outer[&index+6..]);
+                        is_upper = false;
+                        Some(first_out.clone())
+                    }else{
+                        second_out.extend(&buffer_outer[0..&index+6]);
+                        first_out = Vec::<u8>::new();
+                        first_out.extend(&buffer_outer[&index+6..]);
+                        is_upper = true;
+                        Some(second_out.clone())
+                    }
+                } else {
+                    if is_upper {
+                        first_out.extend(&buffer_outer[0..bytes_read]);
+                    }else{
+                        second_out.extend(&buffer_outer[0..bytes_read]);
+                    }
+                    continue;
+                };
+
+                if let Some(x) = &x {
+                    let mut xml_reader = Reader::from_reader(x.as_ref());
+                    // let mut buffer_inner = Vec::new();
+                    loop {
+                        println!("loop inner = ");
+                        match xml_reader.read_event_into(&mut buffer_inner) {
+                            Ok(Event::Start(e)) => {
+                                match e.name().as_ref() {
+                                    b"c" => {
+                                        let mut struct_csv = StructCsv::new();
+                                        for i in e.attributes() {
+                                            match i {
+                                                Ok(x) => {
+                                                    match &x.key.into_inner() {
+                                                        [115u8] | [116u8] => { // b"s", b"t"
+                                                            struct_csv.set_attr(
+                                                                x.key.into_inner()[0].clone());
+                                                        }
+                                                        [114u8] => { // b"r"
+                                                            let a = String::from_utf8_lossy(
+                                                                &*x.clone().value.into_owned()).
+                                                                into_owned();
+                                                            let b = column_to_number(a);
+                                                            struct_csv.set_r_attr_v(b - 1);
+                                                        }
+                                                        _ => {}
+                                                    }
+                                                    s = Some(struct_csv.clone());
+
+                                                }
+                                                Err(_) => {}
+                                            }
+                                        }
+                                    },
+                                    b"v" => is_v = true,
+                                    _ => {},
+                                }
+                            }
+                            Ok(Event::End(e)) => {
+                                match e.name().as_ref() {
+                                    b"row" => {
+                                        let i = row_a.into_iter().map(|a| {
+                                            match a {
+                                                Some(s) => s.clone().
+                                                    get_value(&navi, &name_resolve),
+                                                None => "".to_string()
+                                            }
+                                        }).collect::<Vec<String>>().join(",");
+                                        c_list.push(i);
+                                        count += 1;
+                                        if count == chunk {
+                                            let val = c_list.join("\n");
+                                            tx1.send(val).unwrap();
+                                            count = 0;
+                                            c_list = Vec::new();
+                                        }
+                                        row_a = vec![None; width_len.unwrap()];
+                                    },
+                                    _ => {},
+                                }
+                            }
+                            Ok(Event::Text(e)) => {
+                                if is_v {
+                                    match s {
+                                        Some(ref mut v) => {
+                                            let val = e.unescape().unwrap().into_owned();
+                                            v.set_value(val);
+                                            let i = v.get_r_attr_v();
+                                            row_a[i] = Some(v.clone());
+                                            s = None;
+                                        }
+                                        None => {}
                                     }
-                                }).collect::<Vec<String>>().join(",");
+                                    is_v = false;
+                                }
                                 c_list.push(i);
                                 count += 1;
                                 if count == chunk {
@@ -121,53 +224,38 @@ impl DataGenerator  {
                                 }
                                 row_a = vec![None; width_len.unwrap()];
                             },
-                            _ => {},
-                        }
-                    }
-                    Ok(Event::Text(e)) => {
-                        if is_v {
-                            match s {
-                                Some(ref mut v) => {
-                                    let val = common_match_fn(e)?;
-                                    v.set_value(val);
-                                    let i = v.get_r_attr_v();
-                                    row_a[i] = Some(v.clone());
-                                    s = None;
-                                }
-                                None => {}
+
+                            Ok(Event::Eof) => {
+                                let val = c_list.join("\n");
+                                if let Err(e) = tx1.send(val) {
+                                    let msg = format!("failed to send message: {}", e);
+                                    return Err(PyValueError::new_err(msg));
+                                };
+                                break;
                             }
-                            is_v = false;
-                        }
-                    }
-                    Ok(Event::Eof) => {
-                        let val = c_list.join("\n");
-                        if let Err(e) = tx1.send(val) {
-                            let msg = format!("failed to send message: {}", e);
-                            return Err(PyValueError::new_err(msg));
-                        };
-                        break;
-                    }
-                    Err(e) => {
-                        let msg = format!("something wrong: {}", e);
-                        return Err(PyException::new_err(msg));
-                    }
-                    _ => {
-                        if buffer.starts_with(&dimension_tag){
-                            let dim_tag = String::from_utf8_lossy(&buffer).into_owned();
-                            let dim_tag_contains_colon = dim_tag.contains(':');
-                            let dim_tag_last = if dim_tag_contains_colon {
-                                dim_tag.split(":").last().unwrap()
-                            } else {
-                                let msg = format!("wrong dimension tag");
-                                return Err(PyValueError::new_err(msg));
-                            };
-                            let idx_num = column_to_number(dim_tag_last.to_string())?;
-                            row_a = vec![None; idx_num];
-                            width_len = Some(idx_num);
+                            Err(e) => {
+                                let msg = format!("something wrong: {}", e);
+                                return Err(PyException::new_err(msg));
+                            }
+                            _ => {
+                                if buffer.starts_with(&dimension_tag){
+                                    let dim_tag = String::from_utf8_lossy(&buffer).into_owned();
+                                    let dim_tag_contains_colon = dim_tag.contains(':');
+                                    let dim_tag_last = if dim_tag_contains_colon {
+                                        dim_tag.split(":").last().unwrap()
+                                    } else {
+                                        let msg = format!("wrong dimension tag");
+                                        return Err(PyValueError::new_err(msg));
+                                    };
+                                    let idx_num = column_to_number(dim_tag_last.to_string())?;
+                                    row_a = vec![None; idx_num];
+                                    width_len = Some(idx_num);
+                                }
+                                buffer_inner.clear();
+                            }
                         }
                     }
                 }
-                buffer.clear();
             }
             if let Err(e) = tx1.send(String::from("finish")) {
                 let msg = format!("failed to send message: {}", e);
@@ -232,6 +320,17 @@ fn common_match_fn(e: BytesText) -> Result<String, PyErr> {
         }
     };
     Ok(val)
+}
+
+fn find_vec_index_rev<T: PartialEq>(vector: &[T], sub_vec: &[T]) -> Option<usize> {
+    let s_vec_len = sub_vec.len();
+
+    for i in (s_vec_len..=vector.len()).rev() {
+        if i >= s_vec_len && &vector[(i - &s_vec_len)..i] == sub_vec {
+            return Some(&i - &s_vec_len)
+        }
+    }
+    None
 }
 
 fn column_to_number(s: String) -> Result<usize, PyErr> {
